1202fc7c05d07f888e1dd1feeca281dcd2a8289b
Thanh Nhat Tran <ginthanhnhat@gmail.com>
2025-02-12 22:50:25 +0700

    Done backend for product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -39,6 +39,7 @@ const addProduct = async (req, res) => {
         console.log(product)
 
         res.json({success: true, message: "Product Added!"})
+
     } catch (error) {
         console.log(error)
         res.json({success: false, message: error.message})
@@ -47,21 +48,47 @@ const addProduct = async (req, res) => {
 
 // ========== Function for List product ==========
 const listProduct = async (req, res) => {
+    try {
 
+        const products = await productModel.find({})
+        res.json({success: true, products})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 
     
 }
 
 // ========== Function for Remove product ==========
 const removeProduct = async (req, res) => {
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "Product Removed!"})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message}) 
+    }
 
     
 }
 
 // ========== Function for Single product info ==========
 const singleProduct = async (req, res) => {
+    try {
+        
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        
+        res.json({success: true, product})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message}) 
+    }
 
     
 }
